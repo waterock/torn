3b7c7e624943c32e417d1eb79a4cd7d22982723f
Sulsay <sulsay@gmail.com>
2019-05-31 15:08:15 +0200

    Initial commit for v1.0 of Bazaar Item Link

---
+++ README.md
@@ -0,0 +1,2 @@
+### Torn user scripts
+More info about the ArsonWarehouse project: [Torn forum thread](https://www.torn.com/forums.php#/p=threads&f=67&t=16088273)
---
+++ user-scripts/bazaar-item-market-link.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Bazaar Item Market Link
+// @homepage     https://github.com/sulsay/torn
+// @version      1.0
+// @description  Adds link "View in item market" on expanded items in bazaar
+// @author       Sulsay [2173590]
+// @match        https://www.torn.com/bazaar.php*
+// @grant        none
+// @require      https://arsonwarehouse.com/js/functions/truthy.js
+// ==/UserScript==
+
+(async function () {
+    const itemsList = await truthy(() => document.querySelector('.bazaar-main-wrap .items-list'));
+    new MutationObserver(insertItemPageLinkOnItemExpanded.bind(null, itemsList)).observe(itemsList, {childList: true});
+})();
+
+function insertItemPageLinkOnItemExpanded(itemsList, mutations) {
+    const addedItemInfoMutation = mutations.find(mutation => {
+        return mutation.addedNodes.length === 1 && mutation.addedNodes[0].classList.contains('show-item-info');
+    });
+    if (! addedItemInfoMutation) {
+        return;
+    }
+
+    insertItemPageLink(
+        Array.from(itemsList.children).find(li => li.classList.contains('act')),
+        addedItemInfoMutation.addedNodes[0]
+    );
+}
+
+async function insertItemPageLink(expandedItemLi, itemInfoLi) {
+    const expandedItemId = expandedItemLi.querySelector('[itemid]').getAttribute('itemid');
+
+    const itemInfoSpan = await truthy(() => itemInfoLi.querySelector('.item-cont .item-wrap .info-msg'));
+    const shortDescriptionDiv = itemInfoSpan.firstElementChild;
+
+    const anchor = document.createElement('a');
+    anchor.href = 'https://www.torn.com/imarket.php#/p=shop&type=' + expandedItemId;
+    anchor.style.paddingLeft = '1rem';
+    anchor.style.backgroundImage = 'url("https://arsonwarehouse.com/images/awh-icon-48.png")';
+    anchor.style.backgroundRepeat = 'no-repeat';
+    anchor.style.backgroundSize = '.75rem';
+    anchor.style.backgroundPositionX = '.125rem';
+    anchor.style.color = '#B53471';
+    anchor.style.textDecoration = 'none';
+    anchor.innerText = 'View in item market';
+    shortDescriptionDiv.appendChild(anchor);
+}
